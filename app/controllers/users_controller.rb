1f1b505296339dbbe558d25bc51a48857c70cb91
Chris Tram <chris@mytime.com>
2019-07-14 16:48:17 -0700

    adds users migrate, model and controller

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,19 @@
+class UsersController < ApplicationController
+  protect_from_forgery except: :create
+
+
+  def create
+    puts 'just testings log'
+    email, password = params[:user].values_at :email, :password
+
+    user = User.find_by_email(email)
+
+    if user
+      return render(status: 409, json: { message: 'email_already_taken' })
+    end
+
+    user = User.create(email: email, password: password)
+
+    render(status: 201, json: { user: user.slice(:email, :id) })
+  end
+end
