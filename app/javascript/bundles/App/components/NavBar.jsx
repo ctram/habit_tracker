52e875893f72fbbff01362cd4b0b088d43b45519
Chris Tram <chris@mytime.com>
2019-07-13 23:45:47 -0700

    add react router links

--- app/javascript/bundles/App/components/NavBar.jsx
+++ app/javascript/bundles/App/components/NavBar.jsx
@@ -1,6 +1,8 @@
 import PropTypes from 'prop-types';
 import React from 'react';
 
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+
 export default class NavBar extends React.Component {
   constructor(props) {
     super(props);
@@ -9,7 +11,7 @@ export default class NavBar extends React.Component {
   render() {
     return (
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
-          <a className="navbar-brand" href="#">Navbar</a>
+          <Link className="navbar-brand" href="#" to="/home">Navbar</Link>
           <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
           </button>
@@ -17,7 +19,7 @@ export default class NavBar extends React.Component {
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav mr-auto">
               <li className="nav-item active">
-                <a className="nav-link" href="#">Sign In</a>
+                <Link className="nav-link" href="#" to="/sign-in">Sign In</Link>
               </li>
               {/*
                   <li className="nav-item">
