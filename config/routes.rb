3673f162eae2df703666087c98168d03a075e923
Chris Tram <chris@mytime.com>
2019-07-14 00:23:02 -0700

    redirect all urls to root

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   get 'hello_world', to: 'hello_world#index'
   root 'statics#index'
+
+  get '*foo', to: 'statics#index'
+  # post /.*/, to: 'statics#index'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
