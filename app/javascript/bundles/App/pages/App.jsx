05c57b5cf1b50c7d4b523a8d814fefaa437f4859
Chris Tram <chris@mytime.com>
2019-07-14 00:22:28 -0700

    correct path to sign-in page

--- app/javascript/bundles/App/pages/App.jsx
+++ app/javascript/bundles/App/pages/App.jsx
@@ -2,7 +2,7 @@ import PropTypes from 'prop-types';
 import React from 'react';
 
 import Home from '../components/Home';
-import SignIn from '../components/SignIn';
+import SignIn from './SignIn';
 import NavBar from '../components/NavBar'
 
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
