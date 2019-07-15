221b1738c6f583e3686233bbd8e9d469911dda8b
Chris Tram <chris@mytime.com>
2019-07-14 22:48:11 -0700

    do NOT mutate state; whoops!!!

--- app/javascript/bundles/App/reducers/usersReducer.js
+++ app/javascript/bundles/App/reducers/usersReducer.js
@@ -1,10 +1,9 @@
 import { SET_CURRENT_USER } from '../constants/constants';
 
-export default (state = '', action) => {
+export default (state = { currentUser: null }, action) => {
   switch (action.type) {
     case SET_CURRENT_USER:
-      return action.user;
-      break;
+      return Object.assign({}, state, { currentUser: action.user});
     default:
       return state;
   }
