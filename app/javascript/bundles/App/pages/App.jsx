48b8a84ee17f09ad2355694f55f34a973fe27324
Chris Tram <chris@mytime.com>
2019-07-14 22:48:33 -0700

    use router switch

--- app/javascript/bundles/App/pages/App.jsx
+++ app/javascript/bundles/App/pages/App.jsx
@@ -3,9 +3,9 @@ import React from 'react';
 
 import Home from '../components/Home';
 import SignIn from './SignIn';
-import NavBar from '../components/NavBar'
+import NavBarContainer from '../containers/NavBarContainer';
 
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
 
 export default class App extends React.Component {
   /**
@@ -18,10 +18,12 @@ export default class App extends React.Component {
   render() {
     return (
         <Router>
-            <NavBar />
+          <NavBarContainer />
+          <Switch>
             <Route exact path='/home' component={Home} />
             <Route path='/sign-in' component={SignIn} />
             <Route path='/sign-up' render={() => (<SignIn type="sign-up" />)} />
+          </Switch>
         </Router>
     );
   }
