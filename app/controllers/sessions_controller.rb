7c0558a61a0282ce2f0f54dba2eb4da24e0fdf05
Chris Tram <chris@mytime.com>
2019-07-14 20:49:10 -0700

    adds sessions controller

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,16 @@
+class SessionsController < ApplicationController
+  skip_before_action :verify_authenticity_token, only: :create
+
+  def create
+    email, password = params[:user].values_at :email, :password
+
+    user = User.find_by_email(email)
+
+    if !user || user.password != password
+      return render(status: 401, json: { message: 'IncorrectEmailOrPassword' })
+    end
+
+    session[:user_id] = user.id
+    render(status: 200, json: { user: user.slice(:id, :email) })
+  end
+end
