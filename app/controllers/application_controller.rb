cf3947eb419ab3b570002b4dfac2acbead29c72f
Chris Tram <venus@venuss-MacBook-Pro.local>
2019-07-15 02:00:53 -0700

    adds helper methods to log in a current user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,25 @@
 class ApplicationController < ActionController::Base
+  helper_method :current_user, :login, :who_is_logged_in?
+
+  def current_user
+      if session[:user_id]
+          @current_user ||= User.find(session[:user_id])
+      else
+          @current_user = nil
+      end
+  end
+
+  def login(user)
+    session[:user_id] = user.id
+    @current_user = user
+  end
+
+  def who_is_logged_in?
+    @current_user
+  end
+
+  def logout
+    session[:user_id] = nil
+    @current_user = nil
+  end
 end
