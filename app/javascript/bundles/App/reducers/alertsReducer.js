bb0bb727d36171bb128a446709289ce222ea63cc
Chris Tram <venus@venuss-MacBook-Pro.local>
2019-07-16 14:45:06 -0700

    handler clearAlert action within alert reducer

--- app/javascript/bundles/App/reducers/alertsReducer.js
+++ app/javascript/bundles/App/reducers/alertsReducer.js
@@ -1,11 +1,13 @@
-import { SET_CURRENT_ALERT } from '../constants/constants';
+import * as constants from '../constants/constants';
 
 export default (state = { currentAlert: null }, action) => {
   let { alertType, message } = action;
 
   switch (action.type) {
-    case SET_CURRENT_ALERT:
+    case constants.SET_CURRENT_ALERT:
       return Object.assign({}, state, { currentAlert: { alertType, message }});
+    case constants.CLEAR_CURRENT_ALERT:
+      return Object.assign({}, state, { currentAlert: null });
     default:
       return state;
   }
