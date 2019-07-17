ef6f2b3c12b08150a5122a3e6f6fa9ae74335c03
Chris Tram <venus@venuss-MacBook-Pro.local>
2019-07-16 17:37:39 -0700

    adds create action for habits controller

--- app/controllers/habits_controller.rb
+++ app/controllers/habits_controller.rb
@@ -1,7 +1,19 @@
 class HabitsController < ApplicationController
-  def index
-    return render(status: 204, json: {}) unless current_user
+  before_action :verify_user_logged_in
 
+  def index
     render(status: 200, json: { habits: current_user.habits })
+  end
+
+  def create
+    habit = current_user.habits.create(habit_params)
+
+    render(status: 201, json: { habit: habit })
+  end
+
+  private
+
+  def habit_params
+    params.require(:habit).permit(:title)
   end
 end
