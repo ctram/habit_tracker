3d0be570bf558cd89d64721e057ba8415dd660e2
Chris Tram <chris@mytime.com>
2019-07-19 15:18:36 -0700

    clean up

--- app/javascript/bundles/App/store/appStore.js
+++ app/javascript/bundles/App/store/appStore.js
@@ -2,17 +2,6 @@ import { createStore, applyMiddleware } from 'redux';
 import appReducer from '../reducers/indexReducer';
 import thunk from 'redux-thunk';
 
-// Note: this API requires redux@>=3.1.0
-// const store = createStore(
-//   rootReducer,
-//   applyMiddleware(thunk)
-// );
-//
-
-
-
-
-
 const configureStore = (railsProps) => (
   createStore(appReducer, railsProps, applyMiddleware(thunk))
 );
