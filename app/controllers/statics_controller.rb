90ead2ebd23dd04b882a41f54494a5ffdeef5717
Chris Tram <chris@mytime.com>
2019-07-20 15:13:07 -0700

    if user is logged in, redirect to root page

--- app/controllers/statics_controller.rb
+++ app/controllers/statics_controller.rb
@@ -1,6 +1,12 @@
 class StaticsController < ApplicationController
     layout 'statics'
 
+    skip_before_action :verify_user_logged_in, only: [:sign_in]
+
     def index
+    end
+
+    def sign_in
+      redirect_to '/' if current_user
     end
 end
