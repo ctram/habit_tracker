84d2cc286e1b4e7b61fe2d14d0d962d2767ca798
venus <venus@venuss-MacBook-Pro.local>
2019-07-26 16:31:26 -0700

    adds user update action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,9 @@
 class UsersController < ApplicationController
   protect_from_forgery except: :create
 
-  skip_before_action :verify_user_logged_in
+  skip_before_action :verify_user_logged_in, only: [:create]
 
   def create
-    puts 'just testings log'
     email, password = params[:user].values_at :email, :password
 
     user = User.find_by_email(email)
@@ -16,5 +15,46 @@ class UsersController < ApplicationController
     user = User.create(email: email, password: password)
 
     render(status: 201, json: { user: user.slice(:email, :id) })
+  end
+
+  def update
+    email, new_password, current_password = user_params.values_at :email, :new_password, :current_password
+
+    user = current_user
+
+    keys = user_params.keys
+
+    if email
+      user.email = email
+    end
+
+    if new_password
+      unless user.authenticate(current_password)
+        return render(status: 401, json: { message: 'IncorrectPassword' })
+      end
+
+      user.password = new_password
+    end
+
+    unless user.save
+      message = ''
+
+      user.errors.messages.each do |attr_name, attr_messages|
+        message += attr_messages.reduce('') do |acc, _message|
+          acc += "#{attr_name} #{_message}\n"
+          acc
+        end
+      end
+
+      return render(status: 400, json: { message: message })
+    end
+
+    render(status: 200, json: { user: user.slice(:email, :id) })
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:password, :current_password, :new_password, :email)
   end
 end
