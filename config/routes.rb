8100659dd1686f011a20a5aa583c868211efeca7
venus <venus@venuss-MacBook-Pro.local>
2019-07-25 22:31:03 -0700

    only have user create action for now

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,12 @@
 Rails.application.routes.draw do
   # get 'hello_world', to: 'hello_world#index'
 
-  resources :users do
+  resources :users, only: [:create] do
     resources :habits do
       member do
         post '/update_habit_completed_for_date', to: 'habits#update_habit_completed_for_date'
       end
     end
-
   end
 
   resources :sessions, only: [:create]
